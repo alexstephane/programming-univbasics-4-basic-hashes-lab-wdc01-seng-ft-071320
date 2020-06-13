ba688d882e0256d54f87dafd13254d1205164f73
Alex Ngundji <alexngundji@gmail.com>
2020-06-13 20:56:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  name = { :last_name = 'Grace Hopper'
+  { last_name: 'Grace Hopper'
 end
 
 
